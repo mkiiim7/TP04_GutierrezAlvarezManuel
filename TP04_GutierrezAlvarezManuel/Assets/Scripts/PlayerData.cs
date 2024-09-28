369f8da631cd7da17bc940b59600fa5dc6bf5d64
mkiiim7 <154470894+mkiiim7@users.noreply.github.com>
2024-09-28 14:15:05 -0300

    Scripts Jump no terminados

--- TP04_GutierrezAlvarezManuel/Assets/Scripts/PlayerData.cs
+++ TP04_GutierrezAlvarezManuel/Assets/Scripts/PlayerData.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 
 [CreateAssetMenu(fileName ="PlayerData", menuName = "Player/Data", order = 1)]
+
 public class PlayerData : ScriptableObject 
 {
-    
-    void Start()
-    {
-        
-    }
+    [Header("Movement/Jump")] 
+    public float jumpForce = 10f;
+    public float jumpTime = 0.5f;
+    public float groundDistance = 0.3f;
+    public float jumpTimer;
 
+    public bool isGrounded = false;
+    public bool isJumping = false;
     
-    void Update()
-    {
-        
-    }
+   
 }
---
+++ TP04_GutierrezAlvarezManuel/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] public PlayerData playerData;  //playerData.jumpforce   playerData.groundDistance  playerData.jumpTime
+    [SerializeField] private LayerMask groundedLayer;
+    [SerializeField] Transform feetPos;
+
+
+
+    private void Update()
+    {
+        playerData.isGrounded = Physics2D.OverlapCircle(feetPos.position,playerData.groundDistance,groundedLayer);  // True or false dependiendo si el OVERLAPCIRCLEcirculo toca el piso
+        
+        if (playerData.isGrounded && Input.GetButtonDown("Jump"))
+        {
+            playerData.isJumping = true;
+            rb.velocity = Vector2.up * playerData.jumpForce;
+        }
+        if (playerData.isJumping && Input.GetButton("Jump"))
+        {
+            playerData.isJumping = true;
+            if (playerData.jumpTimer < playerData.jumpTime) 
+            {
+                rb.velocity = Vector2.up * playerData.jumpForce;
+
+                playerData.jumpTimer += Time.deltaTime;
+            }
+            else
+            {
+                playerData.isJumping = false; 
+            }
+        }
+        if (Input.GetButtonUp("Jump"))
+        {
+            playerData.isJumping = false;
+            
+            playerData.jumpTimer = 0 ;
+        }
+
+    }
+}
