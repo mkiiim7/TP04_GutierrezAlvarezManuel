6cdef7fc34a0e0ea9738da79f4406be816e2e810
mkiiim7 <154470894+mkiiim7@users.noreply.github.com>
2024-09-28 12:46:50 -0300

    Creacion de scriptableObject

---
+++ TP04_GutierrezAlvarezManuel/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName ="PlayerData", menuName = "Player/Data", order = 1)]
+public class PlayerData : ScriptableObject 
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
